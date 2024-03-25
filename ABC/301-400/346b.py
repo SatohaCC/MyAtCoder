05ac8ec951c60368e5922424a83bf8f33209e161
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-03-25 23:49:47 +0900

    ABC/301-400/346a.pyとABC/301-400/346b.pyを追加***

---
+++ ABC/301-400/346b.py
@@ -0,0 +1,28 @@
+import io
+import sys
+
+_INPUT = """\
+3 2
+
+
+
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+# ---------------------------------------------------------------------------------------------------------
+#
+#
+# ---------------------------------------------------------------------------------------------------------
+
+W, B = map(int, input().split())
+S_init = "wbwbwwbwbwbw"
+S_size = len(S_init)
+S = S_init * 20
+for i in range(12):
+    T = S[i : i + W + B]
+    if T.count("w") == W:
+        if T.count("b") == B:
+            print("Yes")
+            exit()
+
+print("No")
