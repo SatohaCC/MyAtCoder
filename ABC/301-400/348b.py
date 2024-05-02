f2dd46fac76c5633458bd5742f2604b9b7aa377a
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-05-02 23:51:06 +0900

    ABC/301-400/348b.pyを追加

--- ABC/301-400/348b.py
+++ ABC/301-400/348b.py
@@ -2,7 +2,12 @@ import io
 import sys
 
 _INPUT = """\
-ab
+4
+0 0
+2 4
+5 0
+3 4
+
 
 """
 sys.stdin = io.StringIO(_INPUT)
@@ -13,14 +18,4 @@ sys.stdin = io.StringIO(_INPUT)
 # ---------------------------------------------------------------------------------------------------------
 
 
-from collections import Counter
-
-S = input().strip()
-
-# 文字の出現回数を計算
-counter = Counter(S)
-
-# 0回または2回だけ出現するかどうかをチェック
-is_zero_or_two = all(value == 0 or value == 2 for value in counter.values())
-
-print("Yes" if is_zero_or_two else "No")
+N = int(input())
