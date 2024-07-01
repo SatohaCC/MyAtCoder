fa9be211bccef35a92dd2819eee1c69f0ffe67fb
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-07-01 22:12:23 +0900

    style

--- DP/ABC48C.py
+++ DP/ABC48C.py
@@ -7,22 +7,21 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 
 
-
 # ---------------------------------------------------------------------------------------------------------
 
-mod =998244353
+mod = 998244353
 
-n,m,k = map(int,input().split())
+n, m, k = map(int, input().split())
 
-dp=[[0]*(k+1) for i in range(n+1)]
+dp = [[0] * (k + 1) for i in range(n + 1)]
 print(dp)
 dp[0][0] = 1
 
 for i in range(n):
     for j in range(k):
-        for k in range(1,m+1):
-            if k + j <=k:
-                print(i,j)
-                dp[i+1][j+1] += dp[i][j]
+        for k in range(1, m + 1):
+            if k + j <= k:
+                print(i, j)
+                dp[i + 1][j + 1] += dp[i][j]
 
 print(dp[n][k])
